02d45d4465245dbd5fa5f4957b804c2377e05866
Victor Santos <viictor.amicoo@gmail.com>
2022-09-29 14:57:00 -0300

    Commit inicial

---
+++ exer_77.py
@@ -0,0 +1,11 @@
+'''
+crie um programa que tenha uma tupla com varias palavras(não pode ter acentos). Depois disso, você deve mostrar, para cada palavra a sua vogais
+'''
+palavra = ('VEM','TOMA','E','LUGAR','AQUI','CINEMA','PARALELOGRAMA','POLIGONO','CANETA','CANETA','CELULAR','CARREGADOR','BALA','ANA')
+
+for i in palavra:
+    print(f'\nNa Palavra {i} temos', end=' ')
+    for letra in i:
+        if letra.upper() in 'AEIOU':
+            print(letra,end=' ')
+
