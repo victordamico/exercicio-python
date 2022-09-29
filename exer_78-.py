79b98d50818dfc8f373dd6101723f7b4e9da6aed
Victor Santos <viictor.amicoo@gmail.com>
2022-09-29 14:41:24 -0300

    Commit Inicial

---
+++ exer_78-.py
@@ -0,0 +1,13 @@
+lista = [23,45,68,21,45,323,67,67,78]
+print(f'Lista: {lista} ela tem {len(lista)} elementos')
+
+tupla = 23,45,68,21,45,323,67,67,78
+print(f'Tupla: {lista} ela tem {len(tupla)} elementos')
+
+dicionario = {}.fromkeys([23,45,68,21,45,323,67,67,78], 'discionario')
+print(f'Tupla: {dicionario} ela tem {len(dicionario)} elementos')
+
+conjunto = {23,45,68,21,45,323,67,67,78}
+print(f'Lista: {conjunto} ela tem {len(conjunto)} elementos')
+
+
