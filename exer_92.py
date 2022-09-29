7b8d4a03aeaa15cbcf759a7a78ba1fd8120f4cc7
Victor Santos <viictor.amicoo@gmail.com>
2022-09-29 13:24:17 -0300

    Comite inicial

---
+++ exer_92.py
@@ -0,0 +1,6 @@
+'''
+Os alunos da sua Turma fizeram uma fotação para escolher qual dia da semana é melhor para realizar das lives. Desenvolva um programa em que o usuario oinforme a quantidade dde votos que cada um dos 5 dias da semana obtevem, verifique e exiba qual dia foi escolido
+
+'''
+
+int(input(f'Vote para qual dia da Semana você quer a live /n [1] para Segunda-Feira/n [2] para Terça-Feira/n [3] para Quarta-Feira/[4]/n'))
