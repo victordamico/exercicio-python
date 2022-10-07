505e37ed1da436e03bdce688858602194541c48b
Victor Santos <viictor.amicoo@gmail.com>
2022-10-07 14:55:18 -0300

    commit inicia

---
+++ exer_96.py
@@ -0,0 +1,9 @@
+# Criar função que calcule a largura e o comprimento e de a area
+
+
+
+def area ():
+    largura = float(input('Largura (m): '))
+    comprimento = float(input('Comprimento (m): '))
+    area = largura * comprimento
+    print(f'A área de um terreno {largura}X{comprimento} é de {area}m²')
