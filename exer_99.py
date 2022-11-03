4e6ac192ab37803aef35233b77e35bdb30066f88
Victor Santos <viictor.amicoo@gmail.com>
2022-11-03 14:58:52 -0300

    deixar em ordem os competidores

--- exer_99.py
+++ exer_99.py
@@ -1,4 +1,11 @@
-""" Aqui vamos fazer um loop com For utilizando o List Comprehesion"""
+"""Crie uma lista que tenha cada nome de 5 competidores e imprima em ordem aquele que deu mais volta na quadra, utilizando o sorted"""
 
+competidores =[
+    {"competidor": "Calor", "voltas": 4},
+    {"competidor": "Calor", "voltas": 5},
+    {"competidor": "Calor", "voltas": 1},
+    {"competidor": "Calor", "voltas": 2},
+    {"competidor": "Calor", "voltas": 9},
+]
 
-print([x * 10 for x in [1,4,6,8,9]])
+print(sorted(competidores, key= lambda competidor: competidor["voltas"] ))
