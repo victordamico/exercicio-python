2483baf7139c2acb6f3253e63939102b20005d62
Victor Santos <viictor.amicoo@gmail.com>
2023-01-12 11:49:35 -0300

    Commit_inicial

---
+++ exer_100.py
@@ -0,0 +1,9 @@
+def area():
+    print("  Controle de Terreno  ")
+    print("-" * 20)
+    l = float(input('Largura (m): '))
+    c = float(input('Comprimento (m): '))
+    a = l * c 
+    print(f'A Area do Terreno é de {a}')
+
+area()
---
+++ exer_101.py
@@ -0,0 +1,10 @@
+def escreva(texto):
+    tamanho = len(texto) + 2 
+    print('-' * tamanho)
+    print(texto)
+    print('-' * tamanho)
+
+
+
+escreva('oi')
+escreva('ola mundo')
