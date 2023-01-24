8ea1f416f46a3413367d3dd53b69bc5c7e42e856
Victor Santos <viictor.amicoo@gmail.com>
2023-01-24 10:49:20 -0300

    Commit Inicial

---
+++ exer_03.py
@@ -0,0 +1,6 @@
+"""Crie um programa que leia dois número e mostre a soma entre eles."""
+
+n1 = int(input("Digite um valor: "))
+n2 = int(input("Digite outro valor: "))
+
+print(f"A soma entre {n1} e {n2} é {n1 + n2}")
