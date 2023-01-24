e8308b97b0e0d9cefa72d8102c9276a630057628
Victor Santos <viictor.amicoo@gmail.com>
2023-01-24 10:49:38 -0300

    Commit Inicial

---
+++ exer_05.py
@@ -0,0 +1,5 @@
+"""Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor"""
+
+n = int(input("Digite um número: "))
+
+print(f'Analisando o valor {n} o seu antecessor é {n - 1} e seu sucessor é {n + 1 }')
