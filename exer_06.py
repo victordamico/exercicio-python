e493396211189b267f2dd1fb89dfb0326778144b
Victor Santos <viictor.amicoo@gmail.com>
2023-01-24 10:49:42 -0300

    Commit Inicial

---
+++ exer_06.py
@@ -0,0 +1,8 @@
+"Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada."
+
+n = int(input('Digite um número: '))
+
+print(f'O dobro de {n} é {n * 2}')
+print(f'O triplo de {n} é {n * 3}')
+print(f'A raiz de {n} é {(n ** (1/2)):.2} ')
+
