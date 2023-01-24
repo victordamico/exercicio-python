9191f4747f214447549d6c2750a4b8b5ec5c9388
Victor Santos <viictor.amicoo@gmail.com>
2023-01-24 10:49:46 -0300

    Commit Inicial

---
+++ exer_07.py
@@ -0,0 +1,6 @@
+"""Desenvolva um programa que leia as duas notas de um aluno, Calcule e mostre a sua média"""
+
+note1 = float(input('Digite a primeira nota : '))
+note2 = float(input('Digite a segunda nota : '))
+
+print(f' A media de {note1} e {note2} é {((note1 + note2) / 2):.2 }')
