8958f69f0482328f37f829c1d5ff8ddc58b645b8
Victor Santos <viictor.amicoo@gmail.com>
2023-01-24 10:49:56 -0300

    Commit Inicial

---
+++ exer_09.py
@@ -0,0 +1,14 @@
+"""Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada"""
+
+number = int(input('Digite um número para ver sua tabuada: '))
+
+print(f'\n{number} X 1 = {number * 1}')
+print(f'{number} X 2 = {number * 2}')
+print(f'{number} X 3 = {number * 3}')
+print(f'{number} X 4 = {number * 4}')
+print(f'{number} X 5 = {number * 5}')
+print(f'{number} X 6 = {number * 6}')
+print(f'{number} X 7 = {number * 7}')
+print(f'{number} X 8 = {number * 8}')
+print(f'{number} X 9 = {number * 9}')
+print(f'{number} X 10 = {number * 10}\n')
