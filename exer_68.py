3e24e8c26ccd7515ce4212f4b814c721d0ea28a0
Victor Santos <viictor.amicoo@gmail.com>
2023-02-23 14:27:52 -0300

    Commit Inicial

---
+++ exer_68.py
@@ -0,0 +1,3 @@
+"""Faça um programa que jogue par ou impar com o computador. O jogo so  sera interrompido quando o jogador PERDER, mostrando o total de vitorias consecutivas que ele conquistou no final do jogo."""
+
+
