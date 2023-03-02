cf991145c13f526cd8eb989ca8748ce045bd59bf
Victor Santos <viictor.amicoo@gmail.com>
2023-03-02 14:12:42 -0300

    Commit Inicial

--- exer_70.py
+++ exer_70.py
@@ -3,9 +3,9 @@ A) Qual é o total gasto na compra
 B) Quantos produtos custam mais de R$1000
 C) Qual é o nome do produto mais barato. """
 
-lowest_price = 0 
-lowest_price_value = 0
-high_price = " "
+produto_menor = 0 
+menor = 0
+produto_maior = " "
 total = 0 
 
 print('-' * 30)
@@ -16,22 +16,30 @@ while True:
     name = " "
     price = 0 
     response = "S"
+    contador = 0 
     while response == 'S':
         name = input('Nome do produto: ')
         price = float(input('Valor : '))
         response = input('Deseja continuar [S/N]: ').strip().upper()
-        if lowest_price_value < price :
-            lowest_price_value= price
-            lowest_price = name
+
+        contador += 1 
+        if contador == 1 :
+            menor = price
+            produto_menor = name
+        elif price < menor: 
+            menor= price
+            produto_menor = name
+
             #aqui o menor não esta funcionando arrumar amanha
         if price > 1000:
-            high_price = name
+            produto_maior = name
+        
             
 
         total += price
     break
     
 print(f'O total da compra ficou R${total}')
-print(f'Temos {high_price} produto custando mais de R$ 1000')
-print(f'O produto mais barato foi {lowest_price} e custa {lowest_price_value}')
+print(f'Temos {produto_maior} produto custando mais de R$ 1000')
+print(f'O produto mais barato foi {produto_menor} e custa {menor}')
 
