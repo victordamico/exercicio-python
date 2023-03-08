dd172988f1c1dfc101a863be27574d1a17924123
Victor D'Amico <105016790+victordamico@users.noreply.github.com>
2023-03-08 15:43:29 -0300

    Update exer_97.py

--- exer_97.py
+++ exer_97.py
@@ -2,7 +2,6 @@
 """ Criar um função que usar o **kwargs para retornar as cores favoritas de cada pessoa """
 
 
-
 def color (**kwargs):
     for pessoa,cores in kwargs.items():
         print(f'A dor valorita de {pessoa.title()} é {cores.title()}')
